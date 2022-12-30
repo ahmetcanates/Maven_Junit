751187f1c881e990e4540824daaf328b661fa392
Ahmet Can Ates <omerzed54@gmail.com>
2022-12-30 22:00:09 +0300

    30/12/22

--- src/test/java/day07/C05_IFrame.java
+++ src/test/java/day07/C05_IFrame.java
@@ -14,17 +14,28 @@ public class C05_IFrame extends TestBase {
         driver.get("https://testcenter.techproeducation.com/index.php?page=iframe");
 
         //Ana sayfadaki An iframe with a thin black border: metnin black border yazisinin oldugunu test edelim
-        String anIframetext = driver.findElement(By.xpath("//*[text()='An iframe with a thin black border:']")).getText();
-        Assert.assertTrue(anIframetext.contains("black border"));
+        String anIframetext = driver.findElement(By.xpath("//*[.='An iframe with a thin black border:']")).getText();
+        //tag[.='metin'];    Bana bu tagli metni ver.
+        //tag[text()='text']
+        String expectedText = "black border";
+        Assert.assertTrue(anIframetext.contains(expectedText));
 
         //Ayrica ‘Applications lists’ yazisinin sayfada oldugunu test edelim
-        driver.switchTo().frame(driver.findElement(By.xpath("//*[@style='border:1px solid black;']")));
-        WebElement appListText = driver.findElement(By.xpath("//*[text()='Applications lists']"));
-        Assert.assertTrue(appListText.isDisplayed());
+        driver.switchTo().frame(0);//index 0'dan baslar ilk iframe'e gecis yapiyoruz.
+        String icMetin = driver.findElement(By.xpath("//*[text()='Applications lists']")).getText();
+        String expectedText1 = "Applications lists";
+        Assert.assertEquals(expectedText1,icMetin);
+
+        //Alternatif olarak alttaki secenekler de kullanilabilir.
+        //Assert.assertTrue(icMetin.contains(expectedText1));
+        //Assert.assertTrue(driver.findElement(By.xpath("//*[.='Applications lists']")).isDisplayed());
 
         //Son olarak footer daki ‘Powered By’ yazisini varligini test edilip
-        driver.switchTo().parentFrame();
-        WebElement poveredBy = driver.findElement(By.xpath("//*[text()='Povered By']"));
-        Assert.assertTrue(poveredBy.isDisplayed());
+        //Ana sayfadaki 'Povered By' yazisini test etmek istiyorum ama driver hala iframe'in icinde
+        //iframe'in disina cikmak icin driver.swtichTo().defaultContent/parentFrame
+        driver.switchTo().defaultContent();
+        String footerText = driver.findElement(By.xpath("(//footer[@class='blog-footer'][1])//p")).getText();
+        System.out.println(footerText);
+        Assert.assertTrue(footerText.contains("Povered By"));
     }
 }
