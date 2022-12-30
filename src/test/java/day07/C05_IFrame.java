2fd8ee57ec650642e8c7c455aad8357464a0cc3d
Ahmet Can Ates <omerzed54@gmail.com>
2022-12-30 21:14:00 +0300

    30/12/22

--- src/test/java/day07/C05_IFrame.java
+++ src/test/java/day07/C05_IFrame.java
@@ -13,13 +13,16 @@ public class C05_IFrame extends TestBase {
         //Iframe nedir?
         //https://testcenter.techproeducation.com/index.php?page=iframe
         driver.get("https://testcenter.techproeducation.com/index.php?page=iframe");
+
         //Ana sayfadaki An iframe with a thin black border: metnin black border yazisinin oldugunu test edelim
         WebElement anIframetext = driver.findElement(By.xpath("//*[text()='An iframe with a thin black border:']"));
         Assert.assertTrue(anIframetext.isDisplayed());
+
         //Ayrica ‘Applications lists’ yazisinin sayfada oldugunu test edelim
         driver.switchTo().frame(driver.findElement(By.xpath("//*[@style='border:1px solid black;']")));
         WebElement appListText = driver.findElement(By.xpath("//*[text()='Applications lists']"));
         Assert.assertTrue(appListText.isDisplayed());
+
         //Son olarak footer daki ‘Powered By’ yazisini varligini test edilip
         driver.switchTo().parentFrame();
         WebElement poveredBy = driver.findElement(By.xpath("//*[text()='Povered By']"));
