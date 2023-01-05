263dfbb2e459983a9e89c93a929633a367a585d7
Ahmet Can Ates <omerzed54@gmail.com>
2023-01-05 21:14:31 +0300

    05/01/23

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,40 +1,38 @@
 package utilities;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.time.Duration;
-
+import java.util.ArrayList;
+import java.util.List;
 public abstract class TestBase {
-    //TestBase'i abstract yapmamizin sebebi bu sinifin objesini olusturmak istemiyorum
-    //  TestBase t = new TestBase();    -> YAPILAMAZ
-    //Amacim bu sinifi extend etmek ve icindeki hazir methodlari kullanmak
-
-    //driver objesini olustur. Driver ya public ya protected olmali
-    //Sebebi child classlarda gorulebilir olmasi
+    //    TestBase i abstract yapmamizin sebebi bu sinifin objesini olusturmak istemiyorum
+//    TestBase testBase = new TestBase(); -> YAPILAMAZ
+//    Amacim bu sinifi extend etmek ve icindeki hazir metodlari kullanmak
+//    driver objesini olustur. Driver ya public yada protected olmali.
+//    Sebebi child classlarda gorulebilir olmasi
     protected static WebDriver driver;
-
-    //setUp
+    //    setUp
     @Before
-    public void setup() {
+    public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
-    //tearDown
-
+    //    tearDown
     @After
-    public void tearDown() {
+    public void tearDown(){
         driver.quit();
     }
-
     //    MULTIPLE WINDOW:
-    // Bir parametre alir: Gecis yapmak istedigim sayfanin title'i
-    //ORNEK: switchToWindow("New Window")
+//    1 parametre alir : Gecis Yapmak Istedigim sayfanin Title
+//    ORNEK:
+//    driver.get("https://the-internet.herokuapp.com/windows");
+//    switchToWindow("New Window");
+//    switchToWindow("The Internet")
     public static void switchToWindow(String targetTitle) {
         String origin = driver.getWindowHandle();
         for (String handle : driver.getWindowHandles()) {
@@ -45,5 +43,11 @@ public abstract class TestBase {
         }
         driver.switchTo().window(origin);
     }
-
+    //    windowNumber sıfır (0)'dan başlıyor.
+//    index numarasini parametre olarak alir
+//    ve o indexli pencerece gecis yapar
+    public static void switchToWindow2(int windowNumber){
+        List<String> list = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(list.get(windowNumber));
+    }
 }
