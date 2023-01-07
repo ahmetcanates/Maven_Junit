86dd9d5e39fe1bcb8fc4255ccd95582f1c9f94a0
Ahmet Can Ates <omerzed54@gmail.com>
2023-01-08 00:23:34 +0300

    08/01/23

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -2,8 +2,11 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +15,14 @@ public abstract class TestBase {
 //    TestBase testBase = new TestBase(); -> YAPILAMAZ
 //    Amacim bu sinifi extend etmek ve icindeki hazir metodlari kullanmak
 //    driver objesini olustur. Driver ya public yada protected olmali.
-//    Sebebi child classlarda gorulebilir olmasi
+//    Sebepi child classlarda gorulebilir olmasi
     protected static WebDriver driver;
     //    setUp
     @Before
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+//        driver=WebDriverManager.chromedriver().create();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
@@ -51,15 +55,56 @@ public abstract class TestBase {
         List<String> list = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(list.get(windowNumber));
     }
-
     /*   HARD WAIT:
-   @param : second
-*/
+     @param : second
+    */
     public static void waitFor(int seconds){
         try {
             Thread.sleep(seconds*1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+    //    ACTIONS_RIGHT CLICK
+    public static void rightClickOnElementActions(WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.contextClick(element).perform();
+    }
+    //ACTIONS_DOUBLE CLICK
+    public static void doubleClick(WebElement element) {
+        new Actions(driver).doubleClick(element).build().perform();
+    }
+    //    ACTIONS_HOVER_OVER
+    public static void hoverOverOnElementActions(WebElement element) {
+//        Actions actions = new Actions(driver);
+        new Actions(driver).moveToElement(element).perform();
+    }
+    //    ACTIONS_SCROLL_DOWN
+    public static void scrollDownActions() {
+//        Actions actions = new Actions(driver);
+        new Actions(driver).sendKeys(Keys.PAGE_DOWN).perform();
+    }
+    //    ACTIONS_SCROLL_UP
+    public static void scrollUpActions() {
+//        Actions actions = new Actions(driver);
+        new Actions(driver).sendKeys(Keys.PAGE_UP).perform();
+    }
+    //    ACTIONS_SCROLL_RIGHT
+    public static void scrollRightActions(){
+        new Actions(driver).sendKeys(Keys.ARROW_RIGHT).sendKeys(Keys.ARROW_RIGHT).perform();
+    }
+    //    ACTIONS_SCROLL_LEFT
+    public static void scrollLeftActions(){
+        new Actions(driver).sendKeys(Keys.ARROW_LEFT).sendKeys(Keys.ARROW_LEFT).perform();
+    }
+    //    ACTIONS_DRAG_AND_DROP
+    public static void dragAndDropActions(WebElement source, WebElement target) {
+//        Actions actions = new Actions(driver);
+        new Actions(driver).dragAndDrop(source,target).perform();
+    }
+    //    ACTIONS_DRAG_AND_DROP_BY
+    public static void dragAndDropActions(WebElement source, int x, int y) {
+//        Actions actions = new Actions(driver);
+        new Actions(driver).dragAndDropBy(source,x,y).perform();
     }
 }
