7824b24479f6d7a48911108e32f7018e4722934b
Ahmet Can Ates <omerzed54@gmail.com>
2023-01-08 00:22:56 +0300

    08/01/23

---
+++ src/test/java/day11/C03_FileExist.java
@@ -0,0 +1,27 @@
+package day11;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C03_FileExist {
+
+    @Test
+    public void isExistTest() {
+        String userDIR = System.getProperty("user.dir");
+        System.out.println(userDIR);//C:\Users\ahmet\IdeaProjects\B103Maven_Junit
+
+        String userHOME = System.getProperty("user.home");
+        System.out.println(userHOME);//C:\Users\ahmet
+
+        //logo.jpeg indirip MASAUSTUNE kaydedin
+
+        String dosyaYolu = userHOME + "\\OneDrive\\Masaüstü";
+        System.out.println(dosyaYolu);
+
+        boolean isExist = Files.exists(Paths.get(dosyaYolu));
+        Assert.assertTrue(isExist);
+    }
+}
