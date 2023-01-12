0704ca409775232047823984ecc1d6e0e47f7e02
Ahmet Can Ates <omerzed54@gmail.com>
2023-01-12 23:45:25 +0300

    12/01/23

--- src/test/java/homework/HomeWork3.java
+++ src/test/java/homework/HomeWork3.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.support.ui.Select;
 import utilities.TestBase;
 
@@ -17,6 +18,12 @@ public class HomeWork3 extends TestBase {
     static Select select;
     static WebElement amazonDropdown;
     List<WebElement> amazonDropdownOptions;
+
+    String anaHedefTitle;
+
+    String anaHedefFiyat;
+    String babyTitle;
+    String babyFiyat;
 
     @Test
     public void test() {
@@ -62,23 +69,63 @@ public class HomeWork3 extends TestBase {
         WebElement anaHedef = driver.findElement(with(By.xpath("//*[@class='a-section a-spacing-base']")).toRightOf(anaHedefinSolu).above(anaHedefinAlti));
         anaHedef.click();
 
-        String anaHedefTitle = driver.findElement(By.id("productTitle")).getText();
-        String anaHedefFiyat = driver.findElement(By.xpath("//*[@class='a-section a-spacing-micro']")).getText();
+        anaHedefTitle = driver.findElement(By.id("productTitle")).getText();
+        anaHedefFiyat = driver.findElement(By.xpath("//*[@class='a-section a-spacing-micro']")).getText();
         System.out.println("anaHedefTitle = " + anaHedefTitle);
         System.out.println("anaHedefFiyat = " + anaHedefFiyat);
 
         driver.findElement(By.id("add-to-cart-button")).click();//sepete ekledik
     }
 
+    @Test
+    public void test3() {
+        //Test03
+        //yeni bir sekme açarak amazon anasayfaya gidin
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://amazon.com");
 
-    //Test03
-    //yeni bir sekme açarak amazon anasayfaya gidin
-    //dropdown’dan bebek bölümüne secin
-    //bebek puset aratıp bulundan sonuç sayısını yazdırın
-    //sonuç yazsının puset içerdiğini test edin
-    //5-üçüncü ürüne relative locater kullanarak tıklayin
-    //6-title ve fiyat bilgilerini assign edelim ve ürünü sepete ekleyin
+        //dropdown’dan bebek bölümüne secin
+        amazonDropdown = driver.findElement(By.id("searchDropdownBox"));
+        select = new Select(amazonDropdown);
+        select.selectByVisibleText("Baby");
+
+        //bebek puset aratıp bulundan sonuç sayısını yazdırın
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("bebek puset", Keys.ENTER);
+        WebElement result = driver.findElement(By.xpath("(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        String resultText = result.getText();
+        System.out.println(resultText);
+
+        //sonuç yazsının puset içerdiğini test edin
+        Assert.assertTrue(resultText.contains("puset"));
+
+        //5-üçüncü ürüne relative locater kullanarak tıklayin
+        List<WebElement> babyResults = driver.findElements(By.xpath("//*[@class='a-size-medium a-color-base a-text-normal']"));
+        WebElement anaHedefinAlti = babyResults.get(3);
+        WebElement anaHedef = driver.findElement(with(By.xpath("//*[@class='a-size-medium a-color-base a-text-normal']")).above(anaHedefinAlti));
+        anaHedef.click();
+
+        //6-title ve fiyat bilgilerini assign edelim ve ürünü sepete ekleyin
+        babyTitle = driver.findElement(By.id("productTitle")).getText();
+        babyFiyat = driver.findElement(By.xpath("//*[@class='a-section a-spacing-micro']")).getText();
+        System.out.println("anaHedefTitle = " + anaHedefTitle);
+        System.out.println("anaHedefFiyat = " + anaHedefFiyat);
+        driver.findElement(By.id("add-to-cart-button")).click();//sepete ekledik
+
+        driver.findElement(By.xpath("//a[@id='nav-cart']")).click();//sepete tikladik
+
+        String cartTitle = driver.findElement(By.xpath("//span[@class='a-size-base-plus a-color-base sc-product-title sc-grid-item-product-title']")).
+                getText();
+        String cartPrice = driver.
+                findElement(By.xpath("//span[@class='a-size-medium a-color-base sc-price sc-white-space-nowrap sc-product-price a-text-bold']")).
+                getText();
+    }
+
+
+
 
     // Test 4
     //1-sepetteki ürünlerle eklediğimiz ürünlerin aynı olduğunu isim ve fiyat olarak doğrulayın
+
+
+
 }
