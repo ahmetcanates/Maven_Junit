e5f44e632dd74f11dde8c422ceaacdd3a360a1e4
Ahmet Can Ates <omerzed54@gmail.com>
2023-01-13 23:55:43 +0300

    13/01/23

---
+++ src/test/java/day13/C05_WebDriverException.java
@@ -0,0 +1,17 @@
+package day13;
+
+import utilities.TestBase;
+
+public class C05_WebDriverException {
+    /*
+    WEBDRIVEREXCEPTION NE ZAMAN ALIRIZ???
+    - Driver versiyonlari ve browser versiyonlari ayni veya yakin olmadiginda alinir
+    -Driver i kapattiktan sonra tekrar driver i kullanmak istedigimiz durumlarda alinir
+​
+    COZUM NEDIR???
+    - Framework designinin guzel kurulmus olmasi gerekir;
+    orn Driver sinifinda birden fazla driver.quit() olmamali
+​
+     */
+
+}
