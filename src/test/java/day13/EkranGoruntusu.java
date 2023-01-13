b7098a044477a2be2b2e9917e76c0c732b7ebd92
Ahmet Can Ates <omerzed54@gmail.com>
2023-01-13 23:55:49 +0300

    13/01/23

---
+++ src/test/java/day13/EkranGoruntusu.java
@@ -0,0 +1,26 @@
+package day13;
+
+public class EkranGoruntusu {
+     /*
+       Tester olarak ekran görüntüsü neden alınır ?
+          Dökümantasyon için
+             Geçen Test Case'lerin geçtiğini kayda geçirmek
+             Kalan Test Case'lerin de kaldığını/ bug bulunduğunda kayda geçirmek için
+     */
+
+        /*
+            https://www.techproeducation.com sayfasına git
+            LMS Login linkinin sayfada olduğunu test et
+
+            =============================================
+
+            MAC :command+shift 3 hepsini 4 istedigimizi alir hocam 5de resim sheklinde (mac)
+            MAC : Command + Shift + 4 -> Manual olarak ekran goruntusunu al
+            WINDOWS : Windows + Shift + S -> Manual olarak ekran goruntusunu al
+         */
+
+    /*
+       TEST EXECUTION DOCUMENTATION
+          1.Adım: Ana sayfaya git ve LMS Login element'ini test et
+     */
+}
