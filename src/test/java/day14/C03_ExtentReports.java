426d6ded23574ed440a8f3242e2ef6060a579b45
Ahmet Can Ates <omerzed54@gmail.com>
2023-01-14 23:42:01 +0300

    14/01/23

--- src/test/java/day14/C03_ExtentReports.java
+++ src/test/java/day14/C03_ExtentReports.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utilities.TestBase;
 
 import java.text.SimpleDateFormat;
@@ -47,6 +48,39 @@ public class C03_ExtentReports extends TestBase {
         // Raporu projeme ekliyorum
         extentReports.attachReporter(extentHtmlReporter);   //attach = eklemek
 
+        //Extent Test objesini olustur
+        extentTest = extentReports.createTest("Extent Report Login Test","Smoke Test Raporu");  //1 parametre ile de calisabilir
+
+        //TUM AYARLAR BITTI. EXTENT TEST OBJESIYLE LOGLAMA(RAPORA YAZDIRMA) ISLEMINI YAPABILIRIM
+        extentTest.pass("Kullanici ana sayfaya gider");
+        driver.get("https://www.techproeducation.com");
+
+        //LMS SAYFASINA GIDELIM
+        extentTest.pass("Kullanici LMS Sayfasina gider.");
+        driver.findElement(By.linkText("LMS LOGIN")).click();
+
+        //TEST BITTI
+        extentTest.pass("TEST BASARIYLA GERCEKLESTI");
+
+        //        RAPORU GOSTER. RAPORUN OLUSMASI ICIN BU ADIM ZORUNLUDUR
+        extentReports.flush();
+
 
     }
+
+    //Ekran goruntusu alma:
+    //getScreenShotAs metotu ile alinir. Bu metot Selenium dan gelir
+    //getScreenshotAs metotu TakesScreenshot api indan gelir
+    //Selenium 4 den itibaren 2 farklı sekilde ekran goruntusu alınabilir: Tum Sayfa, Özel element
+    //Olusturmus oldugumuz ReusableMetot yardimizya ister tum sayfanin, istersem belirli bir elementin ekran goruntusunu kolaylıkla alabilirim
+
+    //Extent Reports :
+    //Otomasyon raporları almak icin kullanılır.
+    //Extent report ayrı bir API dir. Extent reports seleniumin disindada kullanilabirlar.
+    //Bu API dan gelen metotlar yardımıyla rapor sablonu oluşturabilir.
+
+    //3 class kullandık
+    //ExtentReports -> sablonu olusturu
+    //ExtentHTMLReporter -> sablonu projeye ekler
+    //ExtentTest -> raporlama islemini yapar. Loglari rapora yazdırir.
 }
