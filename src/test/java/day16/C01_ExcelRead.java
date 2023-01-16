6aca05dba41079cbf571fe188448f1df9ca2bdf6
Ahmet Can Ates <omerzed54@gmail.com>
2023-01-16 14:03:41 +0300

    16/01/23

--- src/test/java/day16/C01_ExcelRead.java
+++ src/test/java/day16/C01_ExcelRead.java
@@ -1,11 +1,14 @@
 package day16;
 
 import org.apache.poi.ss.usermodel.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class C01_ExcelRead {
 
@@ -32,5 +35,37 @@ public class C01_ExcelRead {
 
         //O VERIYI YAZDIR
         System.out.println(cell1);
+
+        //1.SATIR 2.SUTUN
+        Cell cell12 = sheet1.getRow(0).getCell(1);
+        System.out.println(cell12);
+
+        //3.satir 1.sutun yazdir ve o verinin France oldugunu test et      //toString methoduyla String'e cevirebiliriz
+        String cell31 = sheet1.getRow(2).getCell(0).toString();
+        System.out.println(cell31);
+        Assert.assertEquals("France",cell31);
+
+        //Excel'deki toplam satir sayisini bul
+        int toplamSatirSayisi = sheet1.getLastRowNum()+1; //son satir numarasi --- int return eder. index 0'dan basliyor sayma sayilari 1 den. +1 ekle
+        System.out.println(toplamSatirSayisi);
+
+        //Kullanilan toplam satir sayisini bul
+        int kullanilanToplamSatirSayisi = sheet1.getPhysicalNumberOfRows();
+        System.out.println(kullanilanToplamSatirSayisi);//1edn basliyor
+
+        //COUNTRY, CAPITALS key ve value leri map'a alip print et
+        //{{USA, D.C}, {FRANCE, PARIS}, ...}
+
+        //Variable olusturalim. Bu variable exceldeki country, capital verilerini tutacak
+        Map<String, String> ulkeBaskentleri = new HashMap<>();
+
+        for(int satirSayisi=1; satirSayisi<kullanilanToplamSatirSayisi; satirSayisi++) {//country keyword unu almamak icin satirsayisini 1'den basladik
+            String country = sheet1.getRow(satirSayisi).getCell(0).toString();
+            String capital = sheet1.getRow(satirSayisi).getCell(1).toString();
+            ulkeBaskentleri.put(country,capital);
+            //ulkeBaskentleri.put(sheet1.getRow(satirSayisi).getCell(0).toString(),sheet1.getRow(satirSayisi).getCell(1).toString());
+        }
+        System.out.println(ulkeBaskentleri);
+
     }
 }
