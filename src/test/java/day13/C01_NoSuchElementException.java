1abcde5f151cbb6311899a14cacd36c963a46fc0
Ahmet Can Ates <omerzed54@gmail.com>
2023-01-19 00:01:47 +0300

    19/01/23

--- src/test/java/day13/C01_NoSuchElementException.java
+++ src/test/java/day13/C01_NoSuchElementException.java
@@ -7,15 +7,15 @@ import utilities.TestBase;
 
 public class C01_NoSuchElementException extends  TestBase {
     @Test
-    public void nuSuchElementException(){
+    public void noSuchElementException(){
         /*
-​
+
          */
 
         driver.get("https://techproeducation.com");
-        // driver.findElement(By.xpath("//input[@type='searchh']")).sendKeys("QA"+ Keys.ENTER);
+        // driver.findElement(By.xpath("//input[@type='searchh']")).sendKeys("QA"+ Keys.ENTER); //* * * * * *
         //YANLIS LOCATE EDILDIGI ICIN org.openqa.selenium.NoSuchElementException HATASI ALDIK
-        //org.openqa.selenium.NoSuchElementException bu exception javadan degil seleniumdan geliyor.
+        //org.openqa.selenium.NoSuchElementException bu exception javadan degil seleniumdan geliyor. // * * * * * *
 
     }
 }
